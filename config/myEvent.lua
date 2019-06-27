79bbbbb76f2729b95c39d192b6c384d80408eac0
liuhuan <summarychm@hotmail.com>
2019-06-27 19:02:05 +0800

    添加休眠前退出微信功能

---
+++ config/myEvent.lua
@@ -0,0 +1,12 @@
+function logoutWechat(eventType)
+  if (eventType == hs.caffeinate.watcher.screensDidSleep) then
+      print("screensDidSleep")
+      hs.osascript.applescript('tell application "WeChat" to quit')
+  elseif (eventType == hs.caffeinate.watcher.screensDidLock) then
+      --锁屏时似乎无法进行屏幕操作
+      --print("screensDidLock")
+      --hs.applescript(script)
+  end
+end
+caffeinateWatcher = hs.caffeinate.watcher.new(logoutWechat)
+caffeinateWatcher:start()
--- init.lua
+++ init.lua
@@ -3,6 +3,7 @@ require "config.window-management"
 require "config.key-binding"
 require "config.weather"
 require "config.myKey"
+require "config.myEvent"
 
 -- -- Private use
 -- if (hs.host.localizedName() == 'kaboom的MacBook Pro') then
